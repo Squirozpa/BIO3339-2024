e4e674ba452494955803f74156848ccca4efe3e2
squirozpa <squirozpa@uc.cl>
2024-03-07 23:40:25 -0300

    V1.2 Agregado, la funcion que convierte la información a el formato PWM

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Sebastián Quiroz BIO3339-2024  
+
 Repositorio de Sebastián Quiroz para curso Bioinformática Avanzada (BIO3339) 2024  
-Propiedad de Sebastián Quiroz para Pontificia Universidad Católica de Chile   
-Contacto Squirozpa@uc.cl  
-Profesor guía: Francisco Melo / fmelo@bio.puc.cl  
+Propiedad de Sebastián Quiroz para Pontificia Universidad Católica de Chile  
+Contacto <Squirozpa@uc.cl>  
+Profesor guía: Francisco Melo / <fmelo@bio.puc.cl>  
--- Script Nucleotidos-PWM/Nuc-PWM.py
+++ Script Nucleotidos-PWM/Nuc-PWM.py
@@ -4,7 +4,7 @@ Script designed to recieve any length nucleotidic data, and create a file of PWM
 
 import sys
 
-"Function to open file and save it as a list"
+"Function that recieves a path to open file and return it as a list"
 
 
 def open_file(file_path):
@@ -20,17 +20,47 @@ def open_file(file_path):
     return nts_list
 
 
+"Function recieves the list of sequences and creates a list of dictionaries with the length of the first string, then returns the count of nts for each position"
+
+
 def counter(nts_list):
-    nts_distribution = [{'A': 0, 'T': 0, 'G': 0, 'C': 0}
+    nts_distribution = [{'A': 0, 'C': 0, 'G': 0, 'T': 0}
                         for n in range(len(nts_list[0]))]
     for line in nts_list:
+        # for each "sequence" (line)
         for i in range(len(line)):
+            # for every position, adds a count to the corresponding nts of that position
             nts_distribution[i][line[i]] += 1
     return nts_distribution
+
+
+"Function that recieves the list already counted and asks for vertical or horizontal PWM, then returns the PWM in the specified format"
+
+
+def pwmer(nts_list):
+    marker = input("Please select vertical or horizontal (H/V) : ")
+
+    if marker.lower() == "v":
+        nts_headers = ["A", "C", "G", "T"]
+        final = f"      {'  '.join(map(str, range(1, len(nts_list) + 1)))}\n"
+        for nts in range(4):
+            nts_check = nts_headers.pop(0)
+            line = nts_check + "   "
+            for position in range(len(nts_list)):
+                line += str(nts_list[position][nts_check]) + "   "
+            final += f"{line}\n"
+
+    elif marker.lower() == "h":
+        final = "   A   C   G   T\n"
+        for posicion in range(len(nts_list)):
+            line = f"{str(posicion + 1)}    {nts_list[posicion]['A']}    {nts_list[posicion]['C']}    {nts_list[posicion]['G']}    {nts_list[posicion]['T']}"
+            final += f"{line}\n"
+    return final
 
 
 if __name__ == "__main__":
     path = "Script Nucleotidos-PWM\marboxes_27nts.fasta"
     caca = open_file(path)
     poto = counter(caca)
-    print((poto))
+    pipi = pwmer(poto)
+    print(pipi)
