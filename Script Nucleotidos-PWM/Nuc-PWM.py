134a957b31a6b4d3c04dad1b3cf73c09993949ea
squirozpa <squirozpa@uc.cl>
2024-03-08 12:57:38 -0300

    V2 script funcional

--- Script Nucleotidos-PWM/Nuc-PWM.py
+++ Script Nucleotidos-PWM/Nuc-PWM.py
@@ -3,11 +3,12 @@ Script designed to recieve any length nucleotidic data, and create a file of PWM
 """
 
 import sys
-
-"Function that recieves a path to open file and return it as a list"
+import os
 
 
 def open_file(file_path):
+    """Function that recieves a path to open file and return it as a list"""
+
     file = open(file_path, "r")
     lines_file = file.read().splitlines(False)
     file.close()
@@ -20,10 +21,9 @@ def open_file(file_path):
     return nts_list
 
 
-"Function recieves the list of sequences and creates a list of dictionaries with the length of the first string, then returns the count of nts for each position"
-
-
 def counter(nts_list):
+    """Function recieves the list of sequences and creates a list of dictionaries with the length of the first string, then returns the count of nts for each position"""
+
     nts_distribution = [{'A': 0, 'C': 0, 'G': 0, 'T': 0}
                         for n in range(len(nts_list[0]))]
     for line in nts_list:
@@ -34,33 +34,50 @@ def counter(nts_list):
     return nts_distribution
 
 
-"Function that recieves the list already counted and asks for vertical or horizontal PWM, then returns the PWM in the specified format"
-
-
 def pwmer(nts_list):
+    """Function that recieves the list already counted and asks for vertical or horizontal PWM, then returns the PWM in the specified format"""
+
     marker = input("Please select vertical or horizontal (H/V) : ")
 
-    if marker.lower() == "v":
+    if marker.lower() == "h":
         nts_headers = ["A", "C", "G", "T"]
-        final = f"      {'  '.join(map(str, range(1, len(nts_list) + 1)))}\n"
+        final = f"PO    {'  '.join(map(str, range(1, len(nts_list) + 1)))}\n"
         for nts in range(4):
+            # to traverse 4 times one for each nts
             nts_check = nts_headers.pop(0)
-            line = nts_check + "   "
+            line = f"{nts_check}    "
             for position in range(len(nts_list)):
-                line += str(nts_list[position][nts_check]) + "   "
+                line += f"{str(nts_list[position][nts_check])}    "
             final += f"{line}\n"
 
-    elif marker.lower() == "h":
-        final = "   A   C   G   T\n"
+    elif marker.lower() == "v":
+        final = "PO    A    C    G   T\n"
         for posicion in range(len(nts_list)):
             line = f"{str(posicion + 1)}    {nts_list[posicion]['A']}    {nts_list[posicion]['C']}    {nts_list[posicion]['G']}    {nts_list[posicion]['T']}"
             final += f"{line}\n"
+
+    else:
+        print("Please use a single letter V/H for vertical or horizontal")
+        return pwmer(nts_list)
     return final
 
 
+def saver(pwm_str):
+    """Function that recieves tha PWM str and saves it as a file with the specified name"""
+
+    file_name = input("Please type the file name to be save as: \n")
+    file = open(f"{file_name}.txt", "w")
+    file.write(pwm_str)
+    return file_name
+
+
 if __name__ == "__main__":
-    path = "Script Nucleotidos-PWM\marboxes_27nts.fasta"
-    caca = open_file(path)
-    poto = counter(caca)
-    pipi = pwmer(poto)
-    print(pipi)
+    path_folder = os.path.dirname(__file__)
+    file_name = sys.argv[1]
+    path = os.path.join(path_folder, file_name)
+
+    file = open_file(path)
+    counted = counter(file)
+    pwmed = pwmer(counted)
+    name = saver(pwmed)
+    print(f"File {name}.txt succesfully created")
--- Script Nucleotidos-PWM/README.md
+++ Script Nucleotidos-PWM/README.md
@@ -1,9 +1,23 @@
 # Script para generar un PWM a partir de datos de nucleotidos  
 
+Script creado por Sebastian Quiroz para Pontificia Universidad Católica de Chile
+Contacto. <squirozpa@uc.cl>
+
+Utilizacion:
+Correr el archivo principal Nuc-PWM.py en una carpeta con el archivo de nucleotidos a procesar, puede estar en formato fasta o raw en la forma: (pyhton) Nuc-PWM.py [nombre_archivo]
+Responder si requiere que el output sea en horizontal o vertical  
+Por último poner el nombre con el cuál se quiere guardar el archivo
+
+Change Log
 V1.0 (6/3)  
-Se crean las funciones principales para procesar la inforación.  
+Se crean las funciones principales para procesar la información.  
 Abre un archivo (falta poder agregarle el argumento por consola). Siempre y cuando los archivos sean fasta, o solo contengan las lineas de nucleotidos, el script puede generar una lista con diccionarios que tienen como llave el nucleotido, y el conteo de ellos en cada posición.
 Falta generar una función para poder generar el PWM, y agrerar los argumentos por consola
 
 V1.1 (7/3)
-Se agrega la función que recibe la información de las otras funciones y escribe en un string en el fotmat PWM, tanto horizontal como vertical.
+Se agrega la función que recibe la información de las otras funciones y escribe en un string en el formato PWM, tanto horizontal como vertical.
+
+V2 (8/3)
+Se agrega la función para guardar el archivo como un txt  
+Se agregan la funcionalidad de correr el el script agregando el file a usar por consola, si bien ahora el programa tiene instrucciones posteriores a correrlo, todas estas opciones se pueden cambiar a argumentos por consola.
+El programa esta completamente funcional, queda a revisión de su funcionalidad práctica.
